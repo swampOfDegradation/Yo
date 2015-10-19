1f9d1a0ba8a581a074a118147b7a5f2b7a6ad3e2
old4gy <fknmad@ukr.net>
2015-10-20 00:02:08 +0300

    optimized mainmenu

--- weedden/main.cpp
+++ weedden/main.cpp
@@ -2,69 +2,80 @@
 #include <iostream>
 #include "button.h"
 
-void Init(sf::Texture t[],button *b[],int size);
-int setButtonHover(button *b[], int hover, int size);
+const int size = 5;
+struct MainMenuButtons {
+	sf::Texture texture[size];
+	button *button[size];
+};
+
+void Init(struct MainMenuButtons *gv);
+int setButtonHover(struct MainMenuButtons *gv, int hover);
+int changeButtonHover(int *counter);
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(400, 400), "Test");
-	//--create variables--
-	const int button_counts = 5;
-	int button_hover = 0;
-	sf::Texture textures[button_counts];
-	button *buttons[button_counts];
-
-	//--init variables--
-	sf::Texture(*ptr_textures)[button_counts] = &textures;
-	button *(*ptr_buttons)[button_counts] = &buttons;
-	Init(*ptr_textures,*ptr_buttons,button_counts);
-	setButtonHover(*ptr_buttons, button_hover, button_counts);
+	sf::RenderWindow window(sf::VideoMode(400, 300), "Test");
+	struct MainMenuButtons variables;
+	int hover = 0;
+	Init(&variables);
+	setButtonHover(&variables, hover);
 
 	//--window main loop--
-	int tick_counter = 1000;
+	int tick_counter = 8;
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 			if (event.type == sf::Event::Closed)
 				window.close();
-		tick_counter++;
-		if (tick_counter>=500) {
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-			{
-				tick_counter = 0;
-				button_hover++;
-				button_hover = setButtonHover(*ptr_buttons, button_hover, button_counts);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-			{
-				tick_counter = 0;
-				button_hover--;
-				button_hover = setButtonHover(*ptr_buttons, button_hover, button_counts);
-			}
+		switch (changeButtonHover(&tick_counter))
+		{
+		case 1: hover++; break;
+		case 2: hover--; break;
 		}
+		hover = setButtonHover(&variables, hover);
 		window.clear();
-		for (int i = 0; i < button_counts;i++)
-			window.draw(buttons[i]->sprite);
+		for (int i = 0; i < size; i++)
+		{
+			window.draw(variables.button[i]->sprite);
+		}
 		window.display();
 	}
 	return 0;
 }
-void Init(sf::Texture t[],button *b[],int size)
-{
-	t[0].loadFromFile("textures\\button_create.png");
-	t[1].loadFromFile("textures\\button_connect.png");
-	t[2].loadFromFile("textures\\button_options.png");
-	t[3].loadFromFile("textures\\button_about.png");
-	t[4].loadFromFile("textures\\button_exit.png");
-	for (int i = 0; i < size; i++)
-		b[i] = new button(t[i], 10, 10 + 39 * i);
-}
-int setButtonHover(button *b[], int hover, int size)
+int setButtonHover(struct MainMenuButtons *gv, int hover)
 {
 	for (int i = 0; i < size; i++)
-		b[i]->setStatus(0);
+		gv->button[i]->setStatus(0);
 	if (hover < 0)hover = size - 1;
 	if (hover >= size)hover = 0;
-	b[hover]->setStatus(1);
+	gv->button[hover]->setStatus(1);
 	return hover;
+}
+void Init(struct MainMenuButtons *gv) {
+	gv->texture[0].loadFromFile("textures\\button_create.png");
+	gv->texture[1].loadFromFile("textures\\button_connect.png");
+	gv->texture[2].loadFromFile("textures\\button_options.png");
+	gv->texture[3].loadFromFile("textures\\button_about.png");
+	gv->texture[4].loadFromFile("textures\\button_exit.png");
+	for (int i = 0; i < size; i++)
+		gv->button[i] = new button(gv->texture[i], 10, 10 + 39 * i);
+}
+int changeButtonHover(int *counter)
+{
+	++*counter;
+	if (*counter >= 750)
+	{
+		if (*counter >= 1000)*counter = 750;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			*counter=0;
+			return 1;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			*counter = 0;
+			return 2;
+		}
+	}
+	return 0;
 }
